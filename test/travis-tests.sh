545aa0457ae7f27a84e93e900eebfb4f257bbc31
Richard Shade <rshade@rightscale.com>
2017-01-05 15:37:12 -0600

    excluding rightlink_scripts and updating packer

--- test/travis-tests.sh
+++ test/travis-tests.sh
@@ -4,6 +4,6 @@ while IFS= read -r -d $'\0' line; do
     shellcheck -e SC1008 "$line"
     let "exit_code += $?"
     echo $exit_code
-done< <(find . -type f -iname "*.sh" -print0)
+done< <(find . -type f -iname "*.sh" -not -path rightlink_scripts -print0)
 echo "Number of Errors: $exit_code"
 exit $exit_code
