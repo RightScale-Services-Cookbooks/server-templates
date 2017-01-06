d36af72042b61f96d946b43b554dff2150b94839
Richard Shade <rshade@rightscale.com>
2017-01-06 11:12:46 -0600

    trying to prune rightlink_scripts

--- test/travis-tests.sh
+++ test/travis-tests.sh
@@ -4,6 +4,6 @@ while IFS= read -r -d $'\0' line; do
     echo "ShellChecking File:$line"
     shellcheck -x -e SC1008 -e SC1091 "$line"
     let "exit_code += $?"
-done< <(find . -type f -iname "*.sh" -not -path rightlink_scripts -print0)
+done< <(find . -type f -iname "*.sh" -not -path "./rightlink_scripts/*" -print0)
 echo "Number of Errors: $exit_code"
 exit $exit_code
