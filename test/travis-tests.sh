e9405e8e0363842eb8c043c018be2ddd39dab4f4
Richard Shade <rshade@rightscale.com>
2017-01-06 14:01:14 -0600

    updating PHP Application yml and travis-test for right_st

--- test/travis-tests.sh
+++ test/travis-tests.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "Starting ShellCheck Tests"
-exit_code=0
+sc_exit_code=0
 while IFS= read -r -d $'\0' line; do
-    echo "ShellChecking File:$line"
-    shellcheck -x -e SC1008 -e SC1091 "$line"
-    let "exit_code += $?"
+  echo "ShellChecking File:$line"
+  shellcheck -x -e SC1008 -e SC1091 "$line"
+  let "sc_exit_code += $?"
 done< <(find . -type f -iname "*.sh" -not -path "./rightlink_scripts/*" -print0)
-echo "Number of ShellCheck Errors: $exit_code"
+echo "Number of ShellCheck Errors: $sc_exit_code"
 
 echo "Starting right_st validation"
 curl -o /tmp/right_st-linux-amd64.tgz https://binaries.rightscale.com/rsbin/right_st/v1/right_st-linux-amd64.tgz
@@ -16,19 +16,25 @@ export PATH=$PATH:/tmp/right_st
 echo "$TRAVIS"
 if [ "$TRAVIS" == true ]; then
 echo "creating travis config"
-cat <<-EOF> /home/travis/.right_st.yml
+cat > /home/travis/.right_st.yml <<-EOF
 login:
   accounts:
     02-ci:
       host: us-4.rightscale.com
       id: $RIGHTST_ACCOUNT
       refresh_token: $RIGHTST_SERVER_TOKEN
-  default_account: 02-pub
+  default_account: 02-ci
 update:
   check: true
 EOF
 fi
 
-right_st config show
-
+rst_exit_code=0
+while IFS= read -r -d $'\0' line; do
+  echo "right_st checking: $line"
+  /tmp/right_st/right_st st validate "$line"
+  let "rst_exit_code += $?"
+done< <(find . -type f -iname '*.yml' -not -path './.travis.yml' -print0)
+echo "Number of right_st errors: $rst_exit_code"
+let exit_code=sc_exit_code+rst_exit_code
 exit $exit_code
