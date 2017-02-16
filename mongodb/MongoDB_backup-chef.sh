f39b37843c111cfe31e3d3ab281b1f68c07190a7
Richard Shade <rshade@rightscale.com>
2017-02-16 08:58:16 -0600

    adding in mongodb template (#17)
    * adding mongodb template
    
    * adding base scripts
    
    * working on beginning mongo scripts
    
    * updating to remove extra inputs
    
    * updating mongo rightscripts
    
    * updating comments and inputs
    
    * adding in RS-Base
    
    * adding in keyfile
    
    * parsing line breaks
    
    * adding db
    
    * adding user/pass attributes . inputs
    
    * fixing up shell files
    
    * setting to backup recipe

---
+++ mongodb/MongoDB_backup-chef.sh
@@ -0,0 +1,118 @@
+#! /usr/bin/sudo /bin/bash
+# ---
+# RightScript Name: MongoDB Backup - chef
+# Description: 'Backup the mongodb volume on secondary nodes'
+# Inputs:
+#   MONGO_REPLICASET:
+#     Category: MongoDB
+#     Description: MongoDB ReplicaSet Name.
+#     Input Type: single
+#     Required: true
+#     Advanced: false
+#   MONGO_USE_STORAGE:
+#     Category: MongoDB
+#     Description: Enables the use of volumes for the Mongodb data store
+#     Input Type: single
+#     Required: true
+#     Advanced: false
+#     Default: text:false
+#   MONGO_VOLUME_NICKNAME:
+#     Category: MongoDB
+#     Description: Name of the Volume.
+#     Input Type: single
+#     Required: true
+#     Advanced: false
+#   MONGO_VOLUME_SIZE:
+#     Category: MongoDB
+#     Description: Size of the Mongo Volume.
+#     Input Type: single
+#     Required: true
+#     Advanced: false
+#   MONGO_VOLUME_FILESYSTEM:
+#     Category: MongoDB
+#     Description: Mongo Volume FileSystem.
+#     Input Type: single
+#     Required: true
+#     Advanced: false
+#     Default: text:ext4
+#   MONGO_VOLUME_MOUNT_POINT:
+#     Category: MongoDB
+#     Description: MongoDB ReplicaSet Name.
+#     Input Type: single
+#     Required: true
+#     Advanced: false
+#     Default: text:/var/lib/mongodb
+#   MONGO_BACKUP_LINEAGE_NAME:
+#     Category: MongoDB
+#     Description: MongoDB ReplicaSet Name.
+#     Input Type: single
+#     Required: true
+#     Advanced: false
+#   MONGO_RESTORE_FROM_BACKUP:
+#     Category: MongoDB
+#     Description: MongoDB ReplicaSet Name.
+#     Input Type: single
+#     Required: true
+#     Advanced: false
+#     Default: text:false
+#   MONGO_RESTORE_LINEAGE_NAME:
+#     Category: MongoDB
+#     Description: MongoDB ReplicaSet Name.
+#     Input Type: single
+#     Required: true
+#     Advanced: false
+# Attachments: []
+# ...
+
+set -e
+
+HOME=/home/rightscale
+export PATH=${PATH}:/usr/local/sbin:/usr/local/bin
+
+/sbin/mkhomedir_helper rightlink
+
+export chef_dir=$HOME/.chef
+mkdir -p $chef_dir
+
+#get instance data to pass to chef server
+instance_data=$(rsc --rl10 cm15 index_instance_session  /api/sessions/instance)
+instance_uuid=$(echo "$instance_data" | rsc --x1 '.monitoring_id' json)
+instance_id=$(echo "$instance_data" | rsc --x1 '.resource_uid' json)
+monitoring_server=$(echo "$instance_data" | rsc --x1 '.monitoring_server' json)
+
+if [ -e $chef_dir/chef.json ]; then
+  rm -f $chef_dir/chef.json
+fi
+
+# add the rightscale env variables to the chef runtime attributes
+# http://docs.rightscale.com/cm/ref/environment_inputs.html
+cat > $chef_dir/chef.json <<-EOF
+{
+  "name": "${HOSTNAME}",
+  "rightscale":{
+    "instance_uuid":"$instance_uuid",
+    "instance_id":"$instance_id"
+  },
+  "apt": {
+    "compile_time_update": "true"
+  },
+  "rs-base": {
+    "collectd_server": "$monitoring_server",
+    "collectd_hostname": "$instance_uuid"
+  },
+  "rsc_mongodb": {
+    "replicaset":"$MONGO_REPLICASET",
+    "use_storage":"$MONGO_USE_STORAGE",
+    "volume_nickname":"$MONGO_VOLUME_NICKNAME",
+    "volume_size":"$MONGO_VOLUME_SIZE",
+    "volume_filesystem":"$MONGO_VOLUME_FILESYSTEM",
+    "volume_mount_point":"$MONGO_VOLUME_MOUNT_POINT",
+    "backup_lineage_name":"$MONGO_BACKUP_LINEAGE_NAME",
+    "restore_from_backup":"$MONGO_RESTORE_FROM_BACKUP",
+    "restore_lineage_name":"$MONGO_RESTORE_LINEAGE_NAME"
+  },
+  "run_list": ["recipe[rsc_mongodb::mongodb_backup]"]
+}
+EOF
+
+chef-client -j $chef_dir/chef.json
