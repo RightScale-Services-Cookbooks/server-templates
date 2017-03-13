a781bf1e6c1acb67065334dad449bd7bccb0e717
Richard Shade <rshade@rightscale.com>
2017-03-13 09:30:28 -0500

    first pass at ps1

---
+++ windows-chef-client/Windows_Install_Chef_Client.ps1
@@ -0,0 +1,28 @@
+# ---
+# RightScript Name: Windows Install Chef Client
+# Description: Install Chef Client
+# Inputs: {}
+# Attachments: []
+# ...
+# Powershell RightScript to install chef client
+
+# Stop and fail script when a command fails.
+$errorActionPreference = "Stop"
+
+if (test-path C:\opscode -PathType Container) {
+  Write-Output "*** Directory C:\opscode already exists, skipping install..."
+  exit 0
+}
+
+$chefDir="C:\chef"
+if (test-path $chefDir -PathType Container) {
+  Write-Output "*** Directory $chefDir already exists, skipping install..."
+  exit 0
+}
+else {
+  Write-Output "*** Creating $chefDir ..."
+  New-Item $chefDir -type directory | Out-Null
+}
+
+Write-Output("*** Installing chef client msi and waiting for the prompt")
+. { iwr -useb https://omnitruck.chef.io/install.ps1 } | iex; install
