f57e872d0fb0a8504077733557cd1d005f07ca92
Curt Wilhelm <cdwilhelm@users.noreply.github.com>
2017-03-17 11:06:18 -0600

    Add azurerm (#20)
    * Initial dev for ARM implementation.
    
    * Updates for ARM; Added AWS Enhanced Networking
    
    * cleaned up remarks
    
    * Updated configuration for ARM
    
    * update info tab
    
    * fix inputs
    
    * Update packer_configure.sh
    
    Deleted an errant hyphen.
    
    * added rl 10.6.0
    
    * renamed chef-server.yml -> server_template.yml
    
    * renamed *.yml -> server_template.yml
    
    * updated rl10.6 and mci's
    
    * reverting to local scripts
    
    * fixed the mci image
    
    * re fixing name, and updating version
    
    *  updated COOKBOOK_VERSION to v1.0.4
    
    * updated scripts for rl10.6
    
    * updated rhel mci's for rl10.6
    
    * updated rhel mci's for rl10.6
    
    * moving to knife ssl fetch
    
    * added ssl_verify_mode
    
    * add ssl_verify_mode
    
    * updated Chef Client Install to ver 5
    
    * Updated description
    
    * getting private_ip from dash
    
    * Initial dev for ARM implementation.
    
    * Updates for ARM; Added AWS Enhanced Networking
    
    * cleaned up remarks
    
    * Updated configuration for ARM
    
    * update info tab
    
    * fix inputs
    
    * update packer
    
    * merge from master
    
    * merge from master
    
    * fix image_id for azure
    
    * always install rl10
    
    * fix AWS issues

--- imagebuilder/packer_build.sh
+++ imagebuilder/packer_build.sh
@@ -6,28 +6,39 @@
 #   Use Packer to build the image
 # Inputs:
 #   AWS_SECRET_KEY:
-#     Input Type: single
 #     Category: AWS
 #     Description: |
 #       AWS Secret Key
+#     Input Type: single
 #     Required: false
 #     Advanced: true
 #     Default: cred:AWS_SECRET_ACCESS_KEY
 #   AWS_ACCESS_KEY:
-#     Input Type: single
 #     Category: AWS
 #     Description: |
 #       AWS Access Key
+#     Input Type: single
 #     Required: false
 #     Advanced: true
 #     Default: cred:AWS_ACCESS_KEY_ID
 #   GOOGLE_PROJECT:
-#     Input Type: single
 #     Category: Google
 #     Description: |
 #       The name of the Google project your Rightscale account is connected to.
+#     Input Type: single
 #     Required: false
 #     Advanced: true
+#   CLOUD:
+#     Category: Cloud
+#     Description: Select the cloud you are creating the image in
+#     Input Type: single
+#     Required: true
+#     Advanced: false
+#     Possible Values:
+#     - text:ec2
+#     - text:google
+#     - text:azurerm
+# Attachments: []
 # ...
 
 PACKER_DIR=/tmp/packer
@@ -36,19 +47,24 @@ cd ${PACKER_DIR}
 ./packer version
 ./packer validate packer.json
 ./packer build -machine-readable packer.json | tee build.log
-image_id=`grep --binary-files=text 'artifact,0,id' build.log | cut -d, -f6 | cut -d: -f2`
-echo $image_id
+image_id=""
 
-test -z "$image_id" && echo "Build failed. See build log at ${PACKER_DIR}/build.log " && exit 1
-
-cloud=`grep --binary-files=text 'artifact,0,builder-id' build.log | cut -d, -f6 | cut -d: -f2 | cut -d. -f2-`
-case "$cloud" in
+case "$CLOUD" in
 "googlecompute")
   image_id="projects/$GOOGLE_PROJECT/images/$image_id"
   ;;
 "softlayer")
   image_id=`grep --binary-files=text 'artifact,0,string' build.log | cut -d, -f6 | grep -o -E "\(.*" | sed 's/(//' | sed 's/)//'`
   ;;
+"azurerm")
+  image_id=`grep --binary-files=text 'artifact,0,string' build.log | awk -F\\\\ '{ print $4 }' | cut -d\   -f2`
+  image_json=`grep --binary-files=text 'artifact,0,string' build.log | awk -F\\\\ '{ print $6 }' | cut -d\   -f2`
+  echo $image_id
+  echo $image_json
+  echo "{\"$image_id\": {}}" | sudo tee -a /root/rightimage_id_list >/dev/null
+  echo "{\"$image_json\": {}}" | sudo tee -a /root/rightimage_id_list >/dev/null
+  ;;
 esac
 
+test -z "$image_id" && echo "Build failed. See build log at ${PACKER_DIR}/build.log " && exit 1
 echo "{\"$image_id\": {}}" | sudo tee /root/rightimage_id_list >/dev/null
