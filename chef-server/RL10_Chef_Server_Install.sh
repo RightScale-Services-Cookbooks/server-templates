f322a4d57fcff1ea52bcf22f8c9973b2f3edc0d9
Ryan J. Geyer <me@ryangeyer.com>
2017-06-14 15:54:57 -0700

    Changed client download URL from www.opscode.com (deprecated long ago) to www.chef.io

--- chef-server/RL10_Chef_Server_Install.sh
+++ chef-server/RL10_Chef_Server_Install.sh
@@ -97,7 +97,7 @@ if [[ ! -z $VERSION ]]; then
 fi
 
 if [ ! -e /usr/bin/chef-client ]; then
-  curl -L https://www.opscode.com/chef/install.sh | sudo bash -s -- $version
+  curl -L https://www.chef.io/chef/install.sh | sudo bash -s -- $version
 fi
 
 chef_dir="/home/rightscale/.chef"
