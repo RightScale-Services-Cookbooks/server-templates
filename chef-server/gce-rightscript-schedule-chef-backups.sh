fc551cfd17f444557f6699547daddd1d278c4ab0
Stefhen Hovland <stefhen@rightscale.com>
2017-09-20 18:03:29 -0400

    Add new rightscripts

---
+++ chef-server/gce-rightscript-schedule-chef-backups.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/sudo /bin/bash
+# ---
+# RightScript Name: Schedule Chef Backups
+# Description: Creates a cron job that kicks off backups via 'Chef Backup'
+# Inputs:
+#   SCHEDULE:
+#     Category: Backup
+#     Description: Cron style time schedule. (Defaults to 11am UTC, 1 11 * * *)
+#     Input Type: single
+#     Required: true
+#     Advanced: false
+#     Default: text:1 11 * * *
+#   GSUTIL_JSON:
+#     Category: Backup
+#     Description: JSON file with gsutil json credentials.
+#     Input Type: single
+#     Required: true
+#     Advanced: false
+#   GCE_PROJECT_NAME:
+#     Category: Backup
+#     Description: Name of the GCE project.
+#     Input Type: single
+#     Required: true
+#     Advanced: false
+# Attachments: []
+# ...
+
+set ex
+
+# Configure gsutil
+if [ -x "$(which yum)" ]; then
+  cat << EOF > /etc/yum.repos.d/google-cloud-sdk.repo
+[google-cloud-sdk]
+name=Google Cloud SDK
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+  yum install -y google-cloud-sdk
+fi
+
+if [ -x "$(which apt-get)" ]; then
+  export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+  echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee /etc/apt/sources.list.d/google-cloud-sdk.list
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+  apt-get update && apt-get install -y google-cloud-sdk
+fi
+
+cat > /root/gsutil.json <<-EOF
+  $GSUTIL_JSON
+EOF
+
+gcloud auth activate-service-account --key-file=/root/gsutil.json
+gloud config set project $GCE_PROJECT_NAME
+rm -f /root/gsutil.json
+
+# Add backup crontab
+if [ -f /etc/cron.d/chef-backup ]; then
+  rm -rf /etc/cron.d/chef-backup
+fi
+
+cat << EOF > /etc/cron.d/chef-backup
+$SCHEDULE root /usr/local/bin/rsc rl10 run_right_script /rll/run/right_script right_script="Chef Backup"
+EOF
---
+++ chef-server/s3-rightscript-chef-backup.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/sudo /bin/bash
+# ---
+# RightScript Name: Chef Backup
+# Description: Backup a snapshot of the Chef environment to S3.
+# Inputs:
+#   CHEF_BACKUP_BUCKET:
+#     Category: Backup
+#     Description: s3 bucket to save backups to.
+#     Input Type: single
+#     Required: true
+#     Advanced: false
+#   CHEF_BACKUP_BUCKET_REGION:
+#     Category: Backup
+#     Description: s3 bucket region.
+#     Input Type: single
+#     Required: true
+#     Advanced: false
+#   AWS_ACCESS_KEY:
+#     Category: Backup
+#     Description: AWS Access Key.
+#     Required: true
+#     Advanced: false
+#   AWS_SECRET_ACCESS_KEY:
+#     Category: Backup
+#     Description: AWS Secret Access Key.
+#     Required: true
+#     Advanced: false
+# Attachments: []
+# ...
+
+TIMESTAMP=`date '+%Y-%m-%d-%H-%M-%S'`
+OPSCODE_PG_USER="opscode-pgsql"
+PG_DUMP_BIN="/opt/opscode/embedded/bin/pg_dumpall"
+PG_DUMP_FILE="/tmp/postgresql-dump-${TIMESTAMP}.gz"
+TAR_BIN=`which tar`
+
+echo "Disabling outside access to Chef server..."
+chef-server-ctl stop opscode-erchef
+ 
+echo "Backing up Chef server database to [${PG_DUMP_FILE}]..."
+cd /tmp
+sudo -E -u ${OPSCODE_PG_USER} bash -c "${PG_DUMP_BIN} -c | gzip --fast > ${PG_DUMP_FILE}"
+ 
+echo "Stopping all Chef server processes..."
+chef-server-ctl stop
+ 
+ETC_OPSCODE_BACKUP="/etc/opscode"
+VAR_OPT_OPSCODE_BACKUP="/var/opt/opscode"
+FULL_BACKUP="/tmp/chef-server-backup-${TIMESTAMP}.tar.gz"
+SHORT_BACKUP=`basename ${FULL_BACKUP}`
+S3_LOCATION="s3://${CHEF_BACKUP_BUCKET}/${SHORT_BACKUP}"
+
+echo "Backing up all Chef server assets..."
+${TAR_BIN} cvfzp ${FULL_BACKUP} ${ETC_OPSCODE_BACKUP} ${VAR_OPT_OPSCODE_BACKUP} ${PG_DUMP_FILE}
+ 
+echo "Removing extra files..."
+rm -f ${PG_DUMP_FILE}
+ 
+echo "Starting all Chef server processes..."
+chef-server-ctl start
+ 
+aws s3 cp ${FULL_BACKUP} ${S3_LOCATION}
+rm ${FULL_BACKUP}
+
+echo "Backup complete!"
+echo "Backup located at: ${S3_LOCATION}"
