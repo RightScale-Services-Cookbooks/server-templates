a0748fba59654da1f3ddb6e112f193e2ef3a9e1d
Stefhen Hovland <stefhen@rightscale.com>
2017-09-21 15:37:00 -0400

    Merge all backups into one script

--- chef-server/schedule_chef_backups.sh
+++ chef-server/schedule_chef_backups.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sudo /bin/bash
 # ---
-# RightScript Name: Schedule GCE Chef Backups
+# RightScript Name: Schedule Chef Backups
 # Description: Creates a cron job that kicks off backups via 'Chef Backup'
 # Inputs:
 #   SCHEDULE:
@@ -10,6 +10,13 @@
 #     Required: true
 #     Advanced: false
 #     Default: text:1 11 * * *
+#   STORAGE_PROVIDER
+#     Category: Backup
+#     Description: AWS or GCE storage backend to copy Chef backups.
+#     Input Type: array
+#     Required: true
+#     Advanced: false
+#     Default: array:["text:AWS","text:"GCE"]
 #   GSUTIL_JSON:
 #     Category: Backup
 #     Description: JSON file with gsutil json credentials.
@@ -37,10 +44,7 @@
 
 set ex
 
-# Configure gsutil
-if [ -x "$(which yum)" ]; then
-  cat << EOF > /etc/yum.repos.d/google-cloud-sdk.repo
-[google-cloud-sdk]
+YUM_REPO="[google-cloud-sdk]
 name=Google Cloud SDK
 baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64
 enabled=1
@@ -48,30 +52,61 @@ gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
-  yum install -y google-cloud-sdk
+"
+
+AWS_CREDS="[default]
+output = text
+region = ${CHEF_BACKUP_BUCKET_REGION}
+aws_access_key_id = ${AWS_ACCESS_KEY}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+"
+
+CRONTAB="$SCHEDULE root /usr/local/bin/rsc rl10 run_right_script /rll/run/right_script right_script=\"Chef Backup\""
+
+if [ "${STORAGE_PROVIDER}" == "GCE" ]; then
+  if [ -x "$(which yum)" ]; then
+    cat > /etc/yum.repos.d/google-cloud-sdk.repo <<-EOF
+			$YUM_REPO
+		EOF
+    yum install -y google-cloud-sdk
+  fi
+
+  if [ -x "$(which apt-get)" ]; then
+    export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+    echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee /etc/apt/sources.list.d/google-cloud-sdk.list
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+    apt-get update && apt-get install -y google-cloud-sdk
+  fi
+
+  cat > /root/gsutil.json <<-EOF
+		$GSUTIL_JSON
+	EOF
+
+  gcloud auth activate-service-account --key-file=/root/gsutil.json
+  gloud config set project $GCE_PROJECT_NAME
+  rm -f /root/gsutil.json
 fi
 
-if [ -x "$(which apt-get)" ]; then
-  export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-  echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee /etc/apt/sources.list.d/google-cloud-sdk.list
-  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-  apt-get update && apt-get install -y google-cloud-sdk
+if [ "${STORAGE_PROVIDER}" == "AWS" ]; then
+  if [ -x "$(which yum)" ]; then
+    yum install -y python2-pip
+    pip install awscli
+  fi
+
+  if [ -x "$(which apt-get)" ]; then
+    apt-get update && apt-get install -y python-pip
+    pip install awscli
+  fi
+
+  rm -rf /root/.aws
+  mkdir /root/.aws
+
+  cat > /root/.aws/credentials <<-EOF
+		$AWS_CREDS
+	EOF
 fi
 
-cat > /root/gsutil.json <<-EOF
-  $GSUTIL_JSON
-EOF
-
-gcloud auth activate-service-account --key-file=/root/gsutil.json
-gloud config set project $GCE_PROJECT_NAME
-rm -f /root/gsutil.json
-
-# Add backup crontab
-if [ -f /etc/cron.d/chef-backup ]; then
-  rm -rf /etc/cron.d/chef-backup
-fi
-
-cat << EOF > /etc/cron.d/chef-backup
-$SCHEDULE root /usr/local/bin/rsc rl10 run_right_script /rll/run/right_script right_script="Chef Backup to GCE"
+rm -f /etc/cron.d/chef-backup
+cat > /etc/cron.d/chef-backup <<-EOF
+	$CRONTAB
 EOF
