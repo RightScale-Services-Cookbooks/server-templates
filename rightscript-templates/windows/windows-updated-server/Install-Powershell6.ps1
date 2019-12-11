72bb1dd148ec54e9895a440acfcb743d542b07b1
Richard Shade <rshade@flexera.com>
2019-12-11 09:38:30 -0600

    adding stuff

---
+++ rightscript-templates/windows/windows-updated-server/Install-Powershell6.ps1
@@ -0,0 +1,9 @@
+# ---
+# RightScript Name: Windows Install Powershell 6
+# Description: Check whether a RightLink upgrade is available and perform the upgrade.
+# Inputs:{}
+# Attachments: []
+# ...
+
+$errorActionPreference = 'stop'
+Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet -AddExplorerContextMenu"
---
+++ rightscript-templates/windows/windows-updated-server/Windows-Update.ps1
@@ -0,0 +1,62 @@
+# ---
+# RightScript Name: RL10 Enable and Run Windows Update
+# Description: Check whether a RightLink upgrade is available and perform the upgrade.
+# Inputs: {}
+# Attachments: []
+# ...
+
+$errorActionPreference = 'stop'
+$WuaClient = "C:\Windows\System32\wuauclt.exe"
+$UsoClient = "C:\Windows\System32\UsoClient.exe"
+function Invoke-WuaClient{
+  Write-Output "Running WuaClient"
+  Start-Process -FilePath $WuaClient -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt -Wait -ArgumentList '/DetectNow','/UpdateNow'
+  Get-Content stdout.txt
+  Get-Content stderr.txt
+}
+
+function Invoke-UsoClient{
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory=$true)]
+    [string]$switch
+  )
+  Write-Output "Running UsoClient with Switch:"$switch
+  Start-Process -FilePath $UsoClient -RedirectStandardOutput stdout.txt -RedirectStandardError stderr.txt -Wait -ArgumentList $switch
+  Get-Content stdout.txt
+  Get-Content stderr.txt
+}
+
+function Start-UsoClient{
+  Invoke-UsoClient "startscan"
+  Invoke-UsoClient "startdownload"
+  Invoke-UsoClient "startinstall"
+}
+
+switch($PSVersionTable.PSVersion.Major){
+  4 {
+    if (Test-Path $WuaClient){
+      Invoke-WuaClient
+    } elseif (Test-Path $UsoClient){
+      Start-UsoClient
+    } else {
+      Write-Output "No Update Clients Found"
+      exit 1
+    }
+  }
+  5 {
+    if (Test-Path $WuaClient){
+      Invoke-WuaClient
+    } elseif (Test-Path $UsoClient){
+      Start-UsoClient
+    } else {
+      Write-Output "No Update Clients Found"
+      exit 1
+    }
+  }
+  6 {
+      Install-Module -Force -SkipPublisherCheck -Scope CurrentUser -Name PSWindowsUpdate
+      Get-WindowsUpdate
+      Install-WindowsUpdate
+  }
+}
