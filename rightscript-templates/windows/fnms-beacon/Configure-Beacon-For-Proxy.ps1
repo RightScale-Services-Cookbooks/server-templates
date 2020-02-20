f0abb3249a79cf7115d86afeb65ba68e6093c32c
Richard Shade <rshade@flexera.com>
2020-02-20 09:28:39 -0600

    Adding in beacon script

--- rightscript-templates/windows/fnms-beacon/Configure-Beacon-For-Proxy.ps1
+++ rightscript-templates/windows/fnms-beacon/Configure-Beacon-For-Proxy.ps1
@@ -1,12 +1,42 @@
 # ---
 # RightScript Name: RL10 Configure Beacon For Proxy
 # Description: Configure Beacon For Proxy
-# Inputs: {}
-# Attachments: 
-# - PSWindowsUpdate.zip
+# Inputs:
+#   BEACON_PROXY:
+#     Category: Beacon
+#     Description: Proxy Server
+#     Input Type: single
+#     Required: false
+#     Advanced: false
+# Attachments: []
 # ...
 
 [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseShouldProcessForStateChangingFunctions", "")]
 Param()
 
 $errorActionPreference = 'stop'
+
+if ( $null -ne $env:BEACON_PROXY ){
+  $https_url = "https://" + $env:BEACON_PROXY
+  $http_url = "http://" + $env:BEACON_PROXY
+
+  Set-ItemProperty -Path "HKLM:SOFTWARE\Wow6432Node\ManageSoft Corp\ManageSoft\Common" `
+    -Type String `
+    -Name "https_proxy" `
+    -Value $https_url
+
+  Set-ItemProperty -Path "HKLM:SOFTWARE\Wow6432Node\ManageSoft Corp\ManageSoft\Common\DownloadSettings\ReplicatorParent" `
+    -Type String `
+    -Name "proxy" `
+    -Value $https_url
+
+  Set-ItemProperty -Path "HKLM:SOFTWARE\Wow6432Node\ManageSoft Corp\ManageSoft\Common\UploadSettings\ReplicatorParent" `
+    -Type String `
+    -Name "proxy" `
+    -Value $https_url
+
+  Set-ItemProperty -Path "HKLM:SOFTWARE\Wow6432Node\ManageSoft Corp\ManageSoft\Launcher\CurrentVersion" `
+    -Type String `
+    -Name "https_proxy" `
+    -Value $http_url
+}
